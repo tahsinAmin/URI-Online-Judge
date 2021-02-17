2ab870a1f0732d6cbdce10ea99b12d4e70b8c45e
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-02-17 20:14:29 +0600

    math - 1197

---
+++ 5. Mathematics/URI_1197_Back_To_Highschool.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <cstring>
+#include <stdio.h>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+#define sc2(a, b) scanf("%d%d", &a, &b)
+#define scan(x) cin>>x
+#define fr(i, a, n) for(int i = (a); i < (n); i++)
+#define println(x) cout<<x<<"\n";
+
+#include <list>
+#include <unordered_map>
+
+int main(){
+    int v,t;
+    while(cin >> v >> t){
+        println(v*t*2);
+    }
+    return 0;
+}

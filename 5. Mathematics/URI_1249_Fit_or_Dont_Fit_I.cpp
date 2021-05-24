d96dd69d227e011d135d64db5af348d7e22db65b
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-05-24 19:21:35 +0600

    math 1240

---
+++ 5. Mathematics/URI_1249_Fit_or_Dont_Fit_I.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+  string s1, s2, result;
+  int dif, test;
+  cin >> test;
+  while (test--)
+  {
+    result = "nao encaixa";
+    cin >> s1 >> s2;
+    dif = s1.length() - s2.length();
+    if (dif >= 0)
+      result = (s1.substr(dif).compare(s2) == 0) ? "encaixa" : result;
+
+    cout << result << "\n";
+  }
+  return 0;
+}

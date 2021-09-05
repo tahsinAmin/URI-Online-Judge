0fbf87f9c7751307d441805a49fe449fbd4fa27a
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-09-05 12:20:47 +0600

    sql 2617 - JOIN

---
+++ 2. Ad-Hoc/URI_1342_Dice.py
@@ -0,0 +1,15 @@
+# First do without traps.
+ 
+no_of_players, no_of_squares = map(int, input().split())
+ 
+players_location = [0]*no_of_players
+t1, t2, t3 = map(int, input().split())
+traps = [t1, t2, t3]
+no_dice_rolls = int(input())
+pos = 0
+for i in range(no_dice_rolls):
+	d1, d2 = map(int, input().split())
+	players_location[pos] += d1 + d2
+	pos = (pos+1) % no_of_players
+ 
+print(no_of_players, no_of_squares, traps, no_dice_rolls, players_location)
---
+++ 9. sql/URI_2617_Provider_ajax_SA.sql
@@ -0,0 +1,4 @@
+SELECT products.name, providers.name 
+FROM providers JOIN products 
+ON providers.id = products.id_providers
+WHERE providers.name = 'Ajax SA';

54a4b8b6881793e85216b4f5a0797238f7a51194
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-10-01 12:52:22 +0600

    beginner 1478

---
+++ 1. Beginner/URI_1478_Square_Matrix_II.py
@@ -0,0 +1,13 @@
+n = int(input())
+x = 0
+while n!=0:
+	for i in range(1,n+1,1):
+		x=i
+		for j in range(1,n+1,1):
+			if i == j:
+				x = 1
+			print(("%3d" % (x)), end="")
+			print("", end=" ") if(j < n) else print()
+			x += (1 if j >= i else -1)
+	print()
+	n = int(input())

ec51edeb86db7583c1d70ebbe1be88da491856b9
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-10-07 19:11:31 +0600

    adhoc 1546

---
+++ 2. Ad-Hoc/URI_1546_Feedback.py
@@ -0,0 +1,9 @@
+feedback = ['q','Rolien', 'Naej', 'Elehcim', 'Odranoel']
+
+n = int(input())
+for x in range(n):
+
+  k = int(input())
+  for y in range(k):
+    inp = int(input())
+    print(feedback[inp])

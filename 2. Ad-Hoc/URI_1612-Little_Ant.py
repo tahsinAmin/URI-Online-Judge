083e7dac6f0a70141eaa02a5d41f3ea50c3bfa25
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-11-03 19:17:14 +0600

    math.ceil for ceiling and import math

---
+++ 2. Ad-Hoc/URI_1612-Little_Ant.py
@@ -0,0 +1,5 @@
+import math
+
+for i in range(int(input())):
+   data = int(input())
+   print(math.ceil(data/2))
--- 5. Mathematics/URI_1585-Making_Kites.py
+++ 5. Mathematics/URI_1585-Making_Kites.py
@@ -1,3 +1,3 @@
 for i in range(int(input())):
    data = list(map(int,input().split()))
-   print(0.5 * data[0]*data[1])
+    print(data[0] * data[1] // 2, "cm2")

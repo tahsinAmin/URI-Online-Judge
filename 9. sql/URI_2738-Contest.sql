336ea8deb4bbf3d8177dbafecaf37f3e5e7d6d98
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-11-14 22:20:06 +0600

    sum,div then .2 => trunc(((math*2)+(pplan*5))/4,2)

--- 9. sql/URI_2738-Contest.sql
+++ 9. sql/URI_2738-Contest.sql
@@ -1,1 +1,4 @@
-SELECT name, math as final_score
+SELECT name, trunc(((score.math*2)+(score.specific*3)+(score.project_plan*5))/10,2) as avg 
+FROM candidate
+JOIN score on score.candidate_id=candidate.id
+ORDER BY avg DESC;

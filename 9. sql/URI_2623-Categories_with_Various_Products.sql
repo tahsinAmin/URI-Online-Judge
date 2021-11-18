81fd85429085df78e71790a3c4136e25239a33c1
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-11-18 12:05:21 +0600

    within given no. of IDs => p.id_c in (1,2,3,6,9);

---
+++ 9. sql/URI_2623-Categories_with_Various_Products.sql
@@ -0,0 +1,4 @@
+SELECT products.name, categories.name
+FROM products JOIN categories
+ON products.id_categories= categories.id
+WHERE products.amount > 100 and products.id_categories in (1,2,3,6,9);

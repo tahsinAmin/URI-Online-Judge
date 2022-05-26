f8dce78a9c124465aed41e16ad8a02e11aed8011
tahsinAmin <muhammadtahsinamin@gmail.com>
2022-05-26 22:49:38 +0600

    Power Move #1

--- 2. Ad-Hoc/bee-2227.py
+++ 2. Ad-Hoc/bee-2227.py
@@ -1,37 +1,21 @@
-# https://www.beecrowd.com.br/judge/en/problems/view/2227
-
-# https://www.youtube.com/watch?v=Hf05vARjcjE
-
-# airports, flights = map(int, input().split())
-# arr = [0]*(flights+1)
-# for i in range(flights):
-#     a1, a2 = map(int, input().split())
-#     arr[a1] += 1
-#     arr[a2] += 1
-# max_val = max(arr)
-# index_of_max_val = arr.index(max_val)
-# print(index_of_max_val)
-
 airports, flights = map(int, input().split())
-cnt = 0
-arr = [0]*(flights+1)
-a1, a2 = map(int, input().split())
-while a1 != a2:
-    cnt += 1
-    arr[a1] += 1
-    arr[a2] += 1
-    if cnt == flights:
-        # do something
-        cnt = 0
-    a1, a2 = map(int, input().split())
-print("Teste 1")
-max_val = max(arr)
-start = 0
-try:
-    while True:
-        max_val_index = arr.index(max_val, start)
-        start = max_val_index
-        print(start, end=' ')
+test = 1
+while airports + flights != 0:
+    arr = [0]*(airports)
+    for _ in range(flights):
+        a1, a2 = map(int, input().split())
+        arr[a1 - 1] += 1
+        arr[a2 - 1] += 1
+
+    max_val = max(arr)
+    times = arr.count(max_val)
+    start = 0
+    print(f"Teste {test}")
+    for _ in range(times):
+        start = arr.index(max_val, start)
+        print(start+1, end=' ')
         start += 1
-except ValueError:
-    print("")
+
+    print("\n")
+    test += 1
+    airports, flights = map(int, input().split())
--- 5. Mathematics/URI_1247 _ Coast_Guard.py
+++ 5. Mathematics/URI_1247 _ Coast_Guard.py
@@ -1,22 +1,22 @@
 import math
 
 while True:
-	try:
-		d, vF, vG = map(int, input().split())
+    try:
+        d, vF, vG = map(int, input().split())
 
-		tF = 12/vF
-		h = math.sqrt(12*12 + d*d)
-		tG = h/vG
-		
-		print("N" if tF < tG else "S")
-	except(EOFError):
-		break
+        tF = 12/vF
+        h = math.sqrt(12*12 + d*d)
+        tG = h/vG
 
-		#  vF  (12) 
-		#    ______
-		#    |		/
-		# 	 |   /
-		# (d)|  / (h)
-		# 	 | /
-		# 	 |/
-		#  vG
+        print("N" if tF < tG else "S")
+    except(EOFError):
+        break
+
+        #    vF  (12)
+        #     ______
+        #    |    /
+        # 	  |   /
+        # (d)|  / (h)
+        # 	  | /
+        # 	  |/
+        #   vG
